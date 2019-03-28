ed199d8ebbf1737a210f46e107475156c1f1a6fd
Christine Stone <christinestone932@gmail.com>
2019-03-29 09:23:40 +1100

    Vuetify tutorial #12 - padding and margin

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="team">
-    <h1>Team</h1>
+    <h1 class="subheading grey--text">Team</h1>
+      <v-container class="my-5">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consectetur debitis earum ipsum nisi non odit optio quo quos repellat. Atque dicta dolor doloremque iste mollitia neque omnis suscipit tempore.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequatur earum, iure officiis reprehenderit voluptas. Animi aspernatur debitis iste necessitatibus nisi non officia omnis quidem sed sunt tempore, voluptates voluptatum!</p>
+      </v-container>
   </div>
 </template>
 
