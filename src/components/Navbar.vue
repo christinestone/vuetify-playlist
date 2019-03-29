eb4139c76dbe1d9cd8ae15276d8424f6ed094037
Christine Stone <christinestone932@gmail.com>
2019-03-29 14:00:14 +1100

    Vuetify tutorial #23 - popups

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -31,7 +31,7 @@
             </v-btn>
         </v-toolbar>
 
-        <v-navigation-drawer app v-model="drawer" class="secondary">
+        <v-navigation-drawer app v-model="drawer" class="primary">
            <v-layout column align-center>
                <v-flex class="mt-5">
                    <v-avatar size="100">
@@ -39,6 +39,10 @@
                    </v-avatar>
                    <p class="white--text subheading mt-1">John</p>
                </v-flex>
+               <v-flex class="mt-4 mb-3">
+                   <Popup></Popup>
+               </v-flex>
+
            </v-layout>
             <v-list>
                 <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
@@ -54,7 +58,11 @@
     </nav>
 </template>
 <script>
+    import Popup from './Popup'
  export default {
+        components: {
+            Popup
+        },
     data() {
         return {
             drawer: false,
