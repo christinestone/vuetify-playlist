1966b81aac339b1b843b63d2010a8254c47faf54
Christine Stone <christinestone932@gmail.com>
2019-03-29 13:09:19 +1100

    Vuetify tutorial #19 - cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,8 +2,24 @@
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
       <v-container class="my-5">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consectetur debitis earum ipsum nisi non odit optio quo quos repellat. Atque dicta dolor doloremque iste mollitia neque omnis suscipit tempore.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequatur earum, iure officiis reprehenderit voluptas. Animi aspernatur debitis iste necessitatibus nisi non officia omnis quidem sed sunt tempore, voluptates voluptatum!</p>
+          <v-layout row wrap>
+              <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                 <v-card flat class="text-xs-center ma-3">
+                     <v-responsive class="pt4">image goes here</v-responsive>
+                     <v-card-text>
+                      <div class="subheading">{{ person.name }}</div>
+                      <div class="grey--text">{{ person.role }}</div>
+                     </v-card-text>
+                     <v-card-actions>
+                         <v-btn flat color="grey">
+                             <v-icon small left>message</v-icon>
+                             <span>Message</span>
+                         </v-btn>
+                     </v-card-actions>
+                 </v-card>
+
+              </v-flex>
+            </v-layout>
       </v-container>
   </div>
 </template>
@@ -12,6 +28,15 @@
 
 
   export default {
-
+    data() {
+        return {
+            team: [
+                { name: 'John', role: 'web dev' },
+                { name: 'Paul', role: 'graphic designer' },
+                { name: 'George', role: 'web dev' },
+                { name: 'Ringo', role: 'sales' }
+            ]
+        }
+    }
   }
 </script>
