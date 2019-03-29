5fceb0281141be036db2a6ceb3d55afe4d3295c1
Christine Stone <christinestone932@gmail.com>
2019-03-29 13:27:44 +1100

    Vuetify tutorial #20 - avatars

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -5,7 +5,11 @@
           <v-layout row wrap>
               <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
                  <v-card flat class="text-xs-center ma-3">
-                     <v-responsive class="pt4">image goes here</v-responsive>
+                     <v-responsive class="pt4">
+                         <v-avatar size="100" class="grey lighten-2 mt-4">
+                             <img :src="person.avatar">
+                         </v-avatar>
+                     </v-responsive>
                      <v-card-text>
                       <div class="subheading">{{ person.name }}</div>
                       <div class="grey--text">{{ person.role }}</div>
@@ -31,10 +35,10 @@
     data() {
         return {
             team: [
-                { name: 'John', role: 'web dev' },
-                { name: 'Paul', role: 'graphic designer' },
-                { name: 'George', role: 'web dev' },
-                { name: 'Ringo', role: 'sales' }
+                { name: 'John', role: 'web dev' , avatar: '/avatar-1.png'},
+                { name: 'Paul', role: 'graphic designer' , avatar: '/avatar-2.png'},
+                { name: 'George', role: 'web dev' , avatar: '/avatar-3.png'},
+                { name: 'Ringo', role: 'sales' , avatar: '/avatar-4.png'}
             ]
         }
     }
